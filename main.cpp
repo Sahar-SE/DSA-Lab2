f9737bc7e0a9aae2358e2e1c3017bfc4c694744d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2026-09-16 12:00:05 +0500

    Add swapping numbers by function and by pointers

--- main.cpp
+++ main.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 using namespace std;
 
+void swapByAddress(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swapByPointerToPointer(int **x, int **y) {
+    int *temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main() {
+    int a = 5, b = 10;
+    cout << "Before: a = " << a << ", b = " << b << endl;
+    swapByAddress(&a, &b);
+    cout << "After: a = " << a << ", b = " << b << endl;
+
+    int *pa = &a;
+    int *pb = &b;
+    int **ppa = &pa;
+    int **ppb = &pb;
+    cout << "Before: *pa = " << *pa << ", *pb = " << *pb << endl;
+    swapByPointerToPointer(ppa, ppb);
+    cout << "After: *pa = " << *pa << ", *pb = " << *pb << endl;
+
     // cout << "Size of int: " << sizeof(int) << " bytes" << endl;
     // cout << "Size of long: " << sizeof(long) << " bytes" << endl;
     // cout << "Size of double: " << sizeof(double) << " bytes" << endl;
     // cout << "Size of float: " << sizeof(float) << " bytes" << endl;
     // cout << "Size of char: " << sizeof(char) << " bytes" << endl;
 
-    int list[5] = {3, 6, 9, 12, 15};
-    int *pArr = list;
+    // int list[5] = {3, 6, 9, 12, 15};
+    // int *pArr = list;
 
-    for (int i = 0; i < 5; i++) {
-        cout << *(pArr + i) << " ";
-    }
+    // for (int i = 0; i < 5; i++) {
+    //     cout << *(pArr + i) << " ";
+    // }
 
-    cout << endl;
+    // cout << endl;
+
+
+
 
     return 0;
 }
