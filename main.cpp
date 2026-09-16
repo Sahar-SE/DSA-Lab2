d73cab90f44451cd7eb8b79ae2293fb6f8918062
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2026-09-16 11:44:18 +0500

    Retriving the list elements with pointer

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 
 int main() {
-    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
-    cout << "Size of long: " << sizeof(long) << " bytes" << endl;
-    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
-    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    // cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    // cout << "Size of long: " << sizeof(long) << " bytes" << endl;
+    // cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    // cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    // cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+
+    int list[5] = {3, 6, 9, 12, 15};
+    int *pArr = list;
+
+    for (int i = 0; i < 5; i++) {
+        cout << *(pArr + i) << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
